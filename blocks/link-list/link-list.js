17ce8f2c063ecf563f80c41b579ffb7108d91073
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-10-25 20:46:48 -0400

    finished link-list

--- blocks/link-list/link-list.js
+++ blocks/link-list/link-list.js
@@ -1,6 +1,9 @@
 export default function decorate(block) {
-  const links = block.querySelectorAll('.button-container');
+  console.log(block.cloneNode(true));
+  const links = block.querySelectorAll('p');
+  // const links = block.querySelectorAll('.button-container');
 
+  console.log(links);
   block.innerHTML = '';
   links.forEach((link) => {
     block.append(link);
