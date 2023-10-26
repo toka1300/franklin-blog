52b172f03b5cbf0284211a651adc77db4b836283
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-10-26 10:26:50 -0400

    Made entire card clickable

--- blocks/cards/cards.js
+++ blocks/cards/cards.js
@@ -1,16 +1,31 @@
 import { createOptimizedPicture } from '../../scripts/aem.js';
 
+const createAnchorElement = (link) => {
+  const aTag = document.createElement('a');
+  aTag.href = link;
+  return aTag;
+};
+
 export default function decorate(block) {
   /* change to ul, li */
   const ul = document.createElement('ul');
   [...block.children].forEach((row) => {
+    const aTag = row.querySelector('a');
+    const link = aTag?.href;
+    aTag?.remove();
+    const newATag = link ? createAnchorElement(link) : '';
     const li = document.createElement('li');
     while (row.firstElementChild) li.append(row.firstElementChild);
     [...li.children].forEach((div) => {
       if (div.children.length === 1 && div.querySelector('picture')) div.className = 'cards-card-image';
       else div.className = 'cards-card-body';
     });
-    ul.append(li);
+    if (newATag) {
+      newATag.append(li);
+      ul.append(newATag);
+    } else {
+      ul.append(li);
+    }
   });
   ul.querySelectorAll('img').forEach((img) => img.closest('picture').replaceWith(createOptimizedPicture(img.src, img.alt, false, [{ width: '750' }])));
   block.textContent = '';
