1b289510966636fbbf7fe359d4c3144126d753b5
Casey Tokarchuk <105597527+toka1300@users.noreply.github.com>
2023-11-02 18:45:55 -0400

    icon upload, styling updates

--- blocks/link-list/link-list.js
+++ blocks/link-list/link-list.js
@@ -1,9 +1,6 @@
 export default function decorate(block) {
-  console.log(block.cloneNode(true));
   const links = block.querySelectorAll('p');
-  // const links = block.querySelectorAll('.button-container');
 
-  console.log(links);
   block.innerHTML = '';
   links.forEach((link) => {
     block.append(link);
